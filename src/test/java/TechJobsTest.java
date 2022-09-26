cb6de6857232f07020a7222ab2d8bd94402d98ce
MamaKit <mama.kit.pyles@gmail.com>
2022-09-26 15:37:32 -0500

    Corrected test code, both tests now pass

--- src/test/java/TechJobsTest.java
+++ src/test/java/TechJobsTest.java
@@ -42,7 +42,7 @@ public class TechJobsTest {
         String input = "0\n2\nBuzzbold\nx";
         String output = runProgramWithInput(input);
         String expected = getFileContents("src/test/resources/testPrintJobs.txt");
-        assertEquals(expected, output);
+        assertEquals(expected.replaceAll("\r\n?", "\n"), output.replaceAll("\r\n?", "\n"));
     }
 
     @Test
@@ -50,7 +50,7 @@ public class TechJobsTest {
         String input = "0\n2\nChicago\nx";
         String output = runProgramWithInput(input);
         String expected = getFileContents("src/test/resources/testPrintJobsNoResults.txt");
-        assertEquals(expected, output);
+        assertEquals(expected.replaceAll("\r\n?", "\n"), output.replaceAll("\r\n?", "\n"));
     }
 
     @Test
